363a36ffb4f4396c0c94278c9694dfe14fadccf3
ronelsmacwanspr <105989300+ronelsmacwanspr@users.noreply.github.com>
2022-05-25 20:48:44 +0530

    Update displayTextConvert.js
    placed const instead of let where ever necessary.

--- displayTextConvert.js
+++ displayTextConvert.js
@@ -1,4 +1,3 @@
-
 const TextTruncator = function displayTextConvert(toReplace , treshold){
 
     if(toReplace.length <= treshold){
@@ -7,14 +6,14 @@ const TextTruncator = function displayTextConvert(toReplace , treshold){
     }
     let resStr="";
 
-    let totChar = toReplace.length;
+    const totChar = toReplace.length;
     
     let toRem = totChar - treshold;
 
     let mid = Math.floor(totChar/2);
     --toRem;
 
-    let cut1 = Math.floor(toRem/2) , cut2 = Math.ceil(toRem/2);
+    const cut1 = Math.floor(toRem/2) , cut2 = Math.ceil(toRem/2);
 
     resStr = toReplace.slice(0,mid-cut1) + ".." + toReplace.slice(mid+cut2,totChar);
 
